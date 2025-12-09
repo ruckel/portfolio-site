a14a28324f11aa44d9e99554839ab959a467e62d
ruckel <kevin.dybeck@gmail.com>
2025-12-09 20:24:09 +0100

    updated .gitignore

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,7 +1,7 @@
 export default [
-  require("/home/korv/nc/repos/portfolio-site/docusaurus/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/korv/nc/repos/portfolio-site/docusaurus/node_modules/infima/dist/css/default/default.css"),
-  require("/home/korv/nc/repos/portfolio-site/docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/korv/nc/repos/portfolio-site/docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/korv/nc/repos/portfolio-site/docusaurus/src/css/custom.css"),
+  require("/home/korv/repos-local/portfolio-site/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/home/korv/repos-local/portfolio-site/node_modules/infima/dist/css/default/default.css"),
+  require("/home/korv/repos-local/portfolio-site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/korv/repos-local/portfolio-site/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/korv/repos-local/portfolio-site/src/css/custom.css"),
 ];
---
+++ docs/index.md
@@ -0,0 +1,6 @@
+---
+sidebar_position: 1
+---
+# Document catalog
+No docs yet.  
+[Go back](/)
